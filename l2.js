9693aedd51c9d1cda2db82f02568bdab096072cd
Mark Moskalevskij <dohaa@ukr.net>
2023-02-14 03:43:38 +0200

    add new programm

---
+++ l2.js
@@ -0,0 +1,7 @@
+let name = prompt('What is your name?')
+alert('Hello, ' + name + ' How are you?');
+
+
+//todo alternative
+// let name = prompt('What is your name?')
+// alert(`Hello, ${name} How are you?`);
